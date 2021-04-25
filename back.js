3ebe6fb4a262b505f6b59321be69b96a9416b73e
sumit <sumitghuraiya@gmail.com>
2021-04-25 21:11:21 +0530

    changet

--- back.js
+++ back.js
@@ -1,10 +1,11 @@
 
 function changeBg()
 {   
-  var images = ['url("images/main.jpg")','url("images/beauty.jpg")','url(images/greg.jpg)'];
+  var images = ['url("images/main.jpg")','url("images/beauty.jpg")','url(images/nasa.jpg)'];
   var header = document.querySelector('header');
   var bg = images[Math.floor(Math.random() * images.length)];
   header.style.backgroundImage = bg ;
 }
 setInterval(changeBg,5000);
+
 
